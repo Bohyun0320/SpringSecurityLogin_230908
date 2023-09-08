c717c95e260e2d49d2b39923039da6d70fdaf65f
Bohyun0320 <etoile0320@naver.ciom>
2023-09-08 14:04:34 +0900

    Initial commit

---
+++ src/main/java/com/bhchoi/springsecuritylogin_230908/SpringSecurityLogin230908Application.java
@@ -0,0 +1,13 @@
+package com.bhchoi.springsecuritylogin_230908;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLogin230908Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLogin230908Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhchoi/springsecuritylogin_230908/SpringSecurityLogin230908ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhchoi.springsecuritylogin_230908;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLogin230908ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
